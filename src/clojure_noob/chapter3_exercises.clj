b6790d0c5a28fc2d625da27d743403a84e950694
raikhan <raikhan@users.noreply.github.com>
2016-05-09 23:06:36 +0800

    started Chapter 4

--- src/clojure_noob/chapter3_exercises.clj
+++ src/clojure_noob/chapter3_exercises.clj
@@ -3,3 +3,9 @@
 
 ;; get doc to the current namespace
 (clojure.core/use '[clojure.repl :only (doc)])
+
+;; write a map function that returns a set
+(defn mapset
+  [func data]
+  (set (map func data)))
+(mapset inc [1 1 2 2])
---
+++ src/clojure_noob/chapter4.clj
@@ -0,0 +1,5 @@
+(ns clojure-noob.chapter4
+  (:gen-class))
+
+;; get doc to the current namespace
+(clojure.core/use '[clojure.repl :only (doc)])
