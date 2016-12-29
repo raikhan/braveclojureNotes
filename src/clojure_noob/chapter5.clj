34f96c8c710c07b3c1c2cdef0fe95e38fb40cc17
raikhan <raikhan@gmail.com>
2016-12-29 21:55:08 +0800

    began chapter 5: functional programming

---
+++ src/clojure_noob/chapter5.clj
@@ -0,0 +1,8 @@
+(ns clojure-noob.chapter5
+  (:gen-class))
+
+;; pure functions have two properties:
+;;
+;; 1. referential transparency - given same input, they always return same output
+;; 2. no side effects
+
